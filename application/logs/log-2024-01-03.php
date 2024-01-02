18ffb88109dbe4029dab8866b40740231f82e306
Deepanshu Mishra <49188002+deepanshu016@users.noreply.github.com>
2024-01-03 01:37:45 +0530

    02-01-2024 Changes

---
+++ application/logs/log-2024-01-03.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2024-01-03 00:28:34 --> Total execution time: 0.0774
+DEBUG - 2024-01-03 00:29:15 --> Total execution time: 0.0735
+DEBUG - 2024-01-03 00:29:19 --> Total execution time: 0.0504
+DEBUG - 2024-01-03 00:29:24 --> Total execution time: 0.1216
+DEBUG - 2024-01-03 00:29:25 --> Total execution time: 0.1124
+DEBUG - 2024-01-03 00:29:34 --> Total execution time: 0.0544
+DEBUG - 2024-01-03 00:29:38 --> Total execution time: 0.0390
+DEBUG - 2024-01-03 00:29:57 --> Total execution time: 0.0526
+DEBUG - 2024-01-03 00:29:59 --> Total execution time: 0.0569
+DEBUG - 2024-01-03 00:30:26 --> Total execution time: 0.0420
+DEBUG - 2024-01-03 00:34:03 --> Total execution time: 0.0856
+DEBUG - 2024-01-03 00:34:05 --> Total execution time: 0.0447
+DEBUG - 2024-01-03 00:35:26 --> Total execution time: 0.0626
+DEBUG - 2024-01-03 00:40:05 --> Total execution time: 0.0548
