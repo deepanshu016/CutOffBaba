ab8422f9ea44b8482751ead933a6690505520403
Mukesh Mishra <mukeshmishra053@gmail.com>
2024-06-11 23:39:25 +0530

    Updated By Mukesh

--- application/config/database.php
+++ application/config/database.php
@@ -6,49 +6,16 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-
-
-
 	'password' => '',
-
-
-
 	'database' => 'cutoff_baba',
-
-
-
 	'dbdriver' => 'mysqli',
-
-
-
 	'dbprefix' => '',
-
-
-
 	'pconnect' => FALSE,
-
-
-
 	'db_debug' => (ENVIRONMENT !== 'production'),
-
-
-
 	'cache_on' => FALSE,
-
-
-
 	'cachedir' => '',
-
-
-
 	'char_set' => 'utf8',
-
-
-
 	'dbcollat' => 'utf8_general_ci',
-
-
-
 	'swap_pre' => '',
 
 
